56f09c525b0110d7bbd6945f4f88feaeb4747c8a
Waweros <lion@waposta.com>
2016-04-24 21:16:48 -0500

    Commented and reformated a lot

--- src/Chess.hs
+++ src/Chess.hs
@@ -60,14 +60,25 @@ Tasks:
  data Color  = Black | White | EmptyColor
    deriving (Eq,Show,Read)
 
+-- Allows us to have a handy reference for the "other side"
  opposite :: Color -> Color
- -- Allows us to have a handy reference for the "other side"
  opposite EmptyColor = EmptyColor
  opposite Black = White
  opposite White = Black
 
+-- Abstraction of an actual chess piece
  data Piece = Piece Color PieceType
    deriving Eq
+
+ instance Show Piece where
+  show (Piece Black p) = map toUpper (show p)
+  show (Piece White p) = show p
+  show (Piece EmptyColor _) = " "
+
+{-
+ The next two functions provide acess to the
+ components of a Piece
+-}
 
  getPieceType :: Piece ->  PieceType
  getPieceType (Piece _ p) = p
@@ -81,31 +92,22 @@ Tasks:
  instance Show Board where
    show (Board a) = printBoard a 7
 
+ -- Prints a board
  printBoard :: Array (Integer,Integer) Piece -> Integer -> String
  printBoard a 0 = "|" ++ concat [ show (a ! (0,y)) ++ "|" | y <- [0..7]] ++ newLine ++ interrupt
  printBoard a x = "|" ++ concat [ show (a ! (x,y)) ++ "|" | y <- [0..7]] ++ newLine ++ interrupt ++ printBoard a (x-1)
 
- instance Show Piece where
-  show (Piece Black p) = map toUpper (show p)
-  show (Piece White p) = show p
-  show (Piece EmptyColor _) = " "
-
+ -- A holder for two tuples makes things easier to interface with
  data Move = Move (Integer,Integer) (Integer,Integer) | NoMove
   deriving Eq
 
  instance Show Move where
    show (Move z z') = "Moving " ++ show z ++ " to " ++ show z'
    show NoMove = "Move? What Move?"
+
  -- A constant for a empty space on the Chess board
  emptyPiece :: Piece
  emptyPiece = Piece EmptyColor EmptyType
-
- unique :: Eq a => [a] -> [a]
- unique = helper []
-   where
-     helper cs [] = cs
-     helper cs (x:xs) | x `elem` cs = helper cs xs
-     helper cs (x:xs) = helper (x:cs) xs
 
  -- A helper function to generate a new board
  pieceNum :: (Eq a,Num a) => a -> PieceType
@@ -130,9 +132,11 @@ Tasks:
  emptyLine :: Integer -> [((Integer,Integer),Piece)]
  emptyLine x = map (\y -> ((x,y),emptyPiece)) [0..7]
 
+ -- A fresh board
  newBoard :: Board
  newBoard = Board $ array ((0,0),(7,7)) $ concat ([linePawns,lineBack] <*> [Black,White]) ++ concatMap emptyLine [2..5]
 
+ -- get a piece from the board
  getPiece :: Board -> (Integer,Integer) -> Piece
  getPiece (Board a) z = a ! z
 
@@ -140,27 +144,35 @@ Tasks:
  movePiece :: Board -> (Integer,Integer) -> (Integer,Integer) -> Board
  movePiece (Board a) z z' = Board ( a // [(z,emptyPiece),(z',a ! z)])
 
+ -- implements Move interface
  makeMove :: Move -> Board -> Board
  makeMove (Move z z') b = movePiece b z z'
  makeMove NoMove b = b
 
+ -- takes a tuple and  checks if it is a valid coordinate
  isValidCoord :: (Integer,Integer) -> Bool
- isValidCoord (x,y) = and $ map (\a -> (a <= 7) && (a >= 0)) [x,y]
+ isValidCoord (x,y) = all (\a -> (a <= 7) && (a >= 0)) [x,y]
 
+ -- given a board and position returns all possible move to there
  whatCanGoThere :: Board -> (Integer,Integer) -> [(Integer,Integer)]
- whatCanGoThere b z = filter (\z' -> checkMove b z' z) (filter (\z' -> isValidCoord z') (map (adder z) deltas))
+ whatCanGoThere b z = filter (\z' -> checkMove b z' z) (filter isValidCoord (map (adder z) deltas))
 
+ -- Tells you wheather a position can be attacked
  attackable :: Board -> (Integer,Integer) -> Bool
- attackable (Board a) z = any (\z' -> getColor (a ! z') == (opposite clr)) (whatCanGoThere (Board a) z)
+ attackable b z = any (\z' -> clrGetter z' == opposite clr) (whatCanGoThere b z)
    where
-    clr = getColor (a ! z)
+    clr = getColor (getPiece b z)
+    clrGetter = getColor . getPiece b
 
+ -- gives back all possible move for a particular board
  getAllMoves :: Board -> [(Move,Board)]
  getAllMoves b = concatMap (getBoards b) (filterBoard isPiece b)
 
+ -- For a particular board and piece gives all possible moves and boards
  getBoards :: Board -> (Integer, Integer) -> [(Move, Board)]
  getBoards b z = map (\z' -> (Move z z',movePiece b z z')) (moves b z)
 
+ -- Is a piece on our side
  isOurSide :: Color -> Piece -> Bool
  isOurSide clr (Piece clr' _) = clr == clr'
 
@@ -168,10 +180,10 @@ Tasks:
  getAllOurMoves c b = filter ((\(Move z _) -> isOurSide c (getPiece b z)) . fst) $ getAllMoves b
 
  isPiece :: Board  -> (Integer,Integer) -> Bool
- isPiece (Board a) = (/= EmptyColor) . getColor . (!) a
+ isPiece b = (/= EmptyColor) . getColor . getPiece b
 
  deltas :: [(Integer,Integer)]
- deltas = unique $ concatMap mult $ [(0,dy)| dy <- [1..7]] ++ [(dx,0)| dx <- [1..7]] ++ [(d,d) | d <- [1..7]] ++ [(1,2),(2,1)]
+ deltas = concatMap mult $ [(0,dy)| dy <- [1..7]] ++ [(dx,0)| dx <- [1..7]] ++ [(d,d) | d <- [1..7]] ++ [(1,2),(2,1)]
 
  moves :: Board -> (Integer,Integer) -> [(Integer,Integer)]
  moves b z = filter (checkMove b z) (filter isValidCoord (map (adder z) deltas))
@@ -180,6 +192,8 @@ Tasks:
  adder (x',y') (x'',y'') = (x'+x'',y'+y'')
 
  mult :: (Integer,Integer) -> [(Integer,Integer)]
+ mult (0,y') = [(0,y'),(0,-y')]
+ mult (x',0) = [(-x',0),(x',0)]
  mult (x',y') = [(-x',y'),(x',-y'),(-x',-y'),(x',y')]
 
  checkMate :: Color -> Board -> Bool
@@ -195,7 +209,7 @@ Tasks:
 
 
  filterBoard :: (Board -> (Integer,Integer) -> Bool) -> Board -> [(Integer,Integer)]
- filterBoard f b@(Board a) = helper (0,0) []
+ filterBoard f b = helper (0,0) []
   where
     helper :: (Integer,Integer) -> [(Integer,Integer)] -> [(Integer,Integer)]
     helper (x,y) xs | (x == 7) && (y == 7) = if f b (x,y) then (x,y):xs else xs
@@ -203,23 +217,23 @@ Tasks:
     helper (x,y) xs = if f b (x,y) then helper (x,y+1) ((x,y):xs) else helper (x,y+1) xs
 
  blckedAt :: Board -> (Integer,Integer) -> (Integer,Integer) -> Maybe (Color,(Integer,Integer))
- blckedAt (Board a) z z'
-  | a ! z == emptyPiece = Nothing
-  | getPieceType (a ! z) == Knight = if EmptyColor /= clr' then Just (clr',z') else Nothing
-  | otherwise = getFirstOnPath (Board a) z z' >>= (\s -> Just (getColor (a ! s),s))
+ blckedAt b z z'
+  | getPiece b z == emptyPiece = Nothing
+  | getPieceType (getPiece b z) == Knight = if EmptyColor /= clr' then Just (clr',z') else Nothing
+  | otherwise = getFirstOnPath b z z' >>= (\s -> Just (getColor (getPiece b s),s))
     where
-      clr' = getColor (a ! z')
+      clr' = getColor (getPiece b z')
 
  getFirstOnPath :: Board -> (Integer,Integer) -> (Integer,Integer) -> Maybe (Integer,Integer)
- getFirstOnPath (Board a) z@(x,y) z'@(x',y') = helper (x+incX,y+incY)
+ getFirstOnPath b z@(x,y) z'@(x',y') = helper (x+incX,y+incY)
     where
       incX = signum dX
       incY = signum dY
       dX = x' - x
       dY = y' - y
       helper p | not (isValidCoord p) = Nothing
-      helper (coordX,coordY) | coordX == x' && coordY == y' = if a ! z' == emptyPiece then Nothing else Just z'
-      helper (coordX,coordY) = if a ! (coordX, coordY) /= emptyPiece
+      helper (coordX,coordY) | coordX == x' && coordY == y' = if getPiece b z' == emptyPiece then Nothing else Just z'
+      helper (coordX,coordY) = if getPiece b (coordX, coordY) /= emptyPiece
                                  then Just (coordX,coordY) else helper (coordX+incX,coordY+incY)
 
  sameOrNot :: Color -> (Integer,Integer) -> Maybe (Color,(Integer,Integer)) -> Bool
@@ -229,13 +243,13 @@ Tasks:
  sameOrNot _ _ _ = True
 
  checkMove :: Board -> (Integer,Integer) -> (Integer,Integer) -> Bool
- checkMove (Board a) z@(x,y) z'@(x',y')
+ checkMove b z@(x,y) z'@(x',y')
   | not (isValidCoord z && isValidCoord z') = False
-  | a ! z == emptyPiece = False
+  | getPiece b z == emptyPiece = False
   | clr == clr' = False
-  | getPieceType (a ! z) == Knight = ((dX == 1) && (dY == 2)) || ((dX == 2) && (dY == 1))
+  | pieceType == Knight = ((dX == 1) && (dY == 2)) || ((dX == 2) && (dY == 1))
   | sameOrNot clr z' blcked = False
-  | getPieceType (a ! z) == King = elem 1 [dX,dY] && (oneDir || eq) && not (check clr (movePiece (Board a) z z'))
+  | pieceType == King = elem 1 [dX,dY] && (oneDir || eq) && not (check clr (movePiece b z z'))
   | pieceType == Queen = oneDir || eq
   | pieceType == Rook = oneDir
   | pieceType == Bishop = eq
@@ -247,10 +261,10 @@ Tasks:
          eq = dX == dY
          dX = abs $ x - x'
          dY = abs $ y - y'
-         pieceType = getPieceType (a ! z)
-         clr = getColor (a ! z)
-         clr' = getColor (a ! z')
-         blcked = blckedAt (Board a) z z'
+         pieceType = getPieceType (getPiece b z)
+         clr = getColor (getPiece b z)
+         clr' = getColor (getPiece b z')
+         blcked = blckedAt b z z'
 
  checkMove _ _ _ = False
 
